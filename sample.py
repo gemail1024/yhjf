96185fedf66d008b83a6eed9818f65cd4072bf38
Carr Darw <gemail1024@gmail.com>
2026-06-16 17:09:46 +0800

    test: 验证标签全中文

--- sample.py
+++ sample.py
@@ -6,7 +6,7 @@ def get_orders(ids):
     for i in ids:
         row = db.query("select * from orders where id=" + str(i))  # 循环内查询
         result.append(row)
-    secret = "ghp_hardcoded_token_abcdef123456"  # 硬编码密钥
+    secret = "ghp_abcdef_hardcoded_999"  # 硬编码密钥
     try:
         do_something()
     except:  # 裸 except
